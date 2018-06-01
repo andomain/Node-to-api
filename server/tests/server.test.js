dd30ff625b47adb356368349a23fa4987ed06462
Sam Anderson <samel.2710@gmail.com>
2018-06-02 00:17:17 +0100

    TEsting GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/Todo');
 
 const testData = [{
+    _id: new ObjectID(),
     text: 'todo1'
 },{
+    _id: new ObjectID(),
     text: 'todo2'
 }]
 
@@ -62,6 +65,31 @@ describe('GET /todos', () => {
             .expect(200)
             .expect((res) => {
                 expect(res.body.todos.length).toBe(testData.length);
-            }).end(done());
+            }).end(done);
     })
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${testData[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(testData[0].text);
+            }).end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${ObjectID(123).toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
 });
